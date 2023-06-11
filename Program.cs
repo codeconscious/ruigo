dc298d7aec253fa55959f7db522289f994e85289
CodeConscious <hello@codeconscio.us>
2023-06-11 16:10:24 +0900

    Cleanup

--- Program.cs
+++ Program.cs
@@ -1,26 +1,17 @@
 ﻿using HtmlAgilityPack;
 using System.Net.Http;
-using System.Net.Http.Headers;
-using System.Threading.Tasks;
-using System.Net;
-using System.Text;
-using System.IO;
 using System.Linq;
 using Spectre.Console;
 using static System.Console;
 
+if (args == null || args.Length != 1)
+{
+    AnsiConsole.WriteLine("一つの日本語の単語を渡してください。");
+    return;
+}
 
-// See https://aka.ms/new-console-template for more information
-
-// if (args == null || args.Length == 0)
-// {
-//     WriteLine("Need a word.");
-//     return;
-// }
-
-// const string fullUrl = "https://thesaurus.weblio.jp/content/%E7%B5%82%E3%82%8F%E3%82%89%E3%81%AA%E3%81%84";
-const string fullUrl = "https://thesaurus.weblio.jp/content/%E5%BF%98%E3%82%8C%E3%82%8B";
-HttpClient client = new();
+var fullUrl = $"https://thesaurus.weblio.jp/content/{args[0]}";
+var client = new HttpClient();
 var response = await client.GetStringAsync(fullUrl);
 
 if (response == null)
@@ -32,28 +23,23 @@ if (response == null)
 var htmlDocument = new HtmlDocument();
 htmlDocument.LoadHtml(response);
 
-var mainDiv = htmlDocument.DocumentNode.SelectSingleNode("//div[@id='main']/div[3]");
-var titles = mainDiv.SelectNodes("//h2[@class='midashigo']"); // There can be multiple.
-foreach (var title in titles)
+var mainDiv = htmlDocument.DocumentNode.SelectSingleNode("//div[@id='main']/div[3]//h2[@class='midashigo']");
+var searchTermHeaders = mainDiv.SelectNodes("//h2[@class='midashigo']"); // There can be multiple.
+foreach (var searchTermHeader in searchTermHeaders)
 {
-    var div = title.ParentNode.SelectNodes("//div");
-    var nextDiv = title.NextSibling.NextSibling;
-    // var table = mainDiv.SelectSingleNode("//table");
-    // var table = mainDiv.SelectSingleNode("//div[@id='main']/div[3]//table");
-    var table = title.NextSibling.NextSibling.ChildNodes[1];
-    // var rows = nextDiv.SelectNodes("//tr");
+    var table = searchTermHeader.NextSibling.NextSibling.ChildNodes[1];
     var rows = table.ChildNodes.Where(cn => cn.Name == "tr");
 
     if (!rows.Any())
         continue;
 
-    WriteLine($"\nTITLE: {title.InnerText}");
+    WriteLine($"\nTITLE: {searchTermHeader.InnerText}");
 
     var spectreTable = new Table();
-    spectreTable.AddColumn("Term");
-    spectreTable.AddColumn("Synonyms");
+    spectreTable.AddColumn("単語");
+    spectreTable.AddColumn("類語");
 
-    foreach (var row in rows)
+    foreach (var row in rows.Skip(1)) // Skip the header row.
     {
         var cells = row.ChildNodes;
         spectreTable.AddRow(cells[0].InnerText, cells[1].InnerText);
